9dec35f8fa6c272ec1bf629c48172c454ba9bd26
AlexandrT <alexandr.taa@gmail.com>
2018-09-30 18:06:37 +0300

    all fixes done

--- Project/js/script.js
+++ Project/js/script.js
@@ -14,12 +14,6 @@ $(document).ready(function () {
         }
     });
 
-    //Form submit block to prevent page refreshing
-
-    $('#submitForm').click(function (e) {
-        //e.preventDefault();
-    });
-
     //AJAX request information about people for TEAM section from randomuser.me
 
     var url = 'https://randomuser.me/api/?results=6&gender=male&nat=us&inc=name,email,phone,cell,picture&noinfo';
@@ -37,7 +31,16 @@ $(document).ready(function () {
             autoplay: true,
             arrows: false,
             slide: ".ajax-slider-item",
-            dots: true
+            dots: true,
+            responsive: [
+                {
+                    breakpoint: 767,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                }
+        ]
         });
 
     });
@@ -46,39 +49,35 @@ $(document).ready(function () {
     });
 
     //Slider Team Members on click details modal window
-    //var sliderItems = $('.ajax-slider-item');
+
     $('.slider-ajax').click(function (e) {
         var modale = $('#teamMemberInfo');
         var modaleMemberDiv = modale.find('.modal-content');
-        modale.addClass('in');
-        modale.modal('show');
-        console.log('click');
-        //var div = e.target;
         var dest = $(e.target);
-        dest = dest.closest('.ajax-slider-item');
-        console.log(dest);
-        //dest = dest.parent().parent();
 
-        var details = {};
-        details.src =  dest.find('img').attr("src");
-        details.name = dest.find('h4').text();
-        details.specialization = dest.find('h5').text();
-        details.email = dest.find('.teamMemberDetails').attr("data-email");
-        details.phone = dest.find('.teamMemberDetails').attr("data-phone");
-        details.cell = dest.find('.teamMemberDetails').attr("data-cell");
-        console.log(details);
-        modaleMemberDiv.find('img').attr("src", dest.find('img').attr("src"));
-        modaleMemberDiv.find('h4').text(dest.find('h4').text());
-        modaleMemberDiv.find('h5').text(dest.find('h5').text());
-        modaleMemberDiv.find('.email').text(dest.find('.teamMemberDetails').attr("data-email"));
-        modaleMemberDiv.find('.phone').text(dest.find('.teamMemberDetails').attr("data-phone"));
-        modaleMemberDiv.find('.cell').text(dest.find('.teamMemberDetails').attr("data-cell"));
+        if ((dest.hasClass('slick-dots'))||(dest.is('button'))) {
+            return;
+        } else {
+            dest = dest.closest('.ajax-slider-item');
+            modale.addClass('in');
+            modale.modal('show');
+            //Alternative way with object for storage of finding results
+/*          var details = {};
+            details.src =  dest.find('img').attr("src");
+            details.name = dest.find('.name').text();
+            details.specialization = dest.find('h5').text();
+            details.email = dest.find('.teamMemberDetails').attr("data-email");
+            details.phone = dest.find('.teamMemberDetails').attr("data-phone");
+            details.cell = dest.find('.teamMemberDetails').attr("data-cell");*/
 
-        //console.log(modaleMemberDiv.find('img'));
-
+            modaleMemberDiv.find('img').attr("src", dest.find('img').attr("src"));
+            modaleMemberDiv.find('h4').text(dest.find('.name').text());
+            modaleMemberDiv.find('h5').text(dest.find('h5').text());
+            modaleMemberDiv.find('.email').text(dest.find('.teamMemberDetails').attr("data-email"));
+            modaleMemberDiv.find('.phone').text(dest.find('.teamMemberDetails').attr("data-phone"));
+            modaleMemberDiv.find('.cell').text(dest.find('.teamMemberDetails').attr("data-cell"));
+        }
     });
-
-
 
     //Isotope layout
 
@@ -96,20 +95,7 @@ $(document).ready(function () {
         autoplay: false,
         arrows: true,
         slide: ".slider-item",
-        //appendDots: $(".dotholder"),
         dots: false
-    });
-
-    //Second slider initiation
-
-    $('.slider-two').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        arrows: false,
-        slide: ".slider-item",
-        //appendDots: $(".dotholder"),
-        dots: true
     });
 
     //Smooth scroll on links animation
@@ -179,25 +165,11 @@ $(document).ready(function () {
             ],
 
             slickItem = document.createElement('div');
-        var template = _.template('<div class="ajax-slider-item"><div  class="img-wrapper">\n' +
-            '            <img class="img-responsive teamMemberPhoto" src="<%- src %>" alt="Team member image">\n' +
-            '            <div class="hover-effect">\n' +
-            '                <div class="text-holder">\n' +
-            '                    <i class="fas fa-hand-point-up"></i>\n' +
-            '                    <h6>Click for details</h6>\n' +
-            '                </div>\n' +
-            '            </div>\n' +
-            '            <div class="hover-green">\n' +
-            '            </div>\n' +
-            '        </div>\n' +
-            '        <h4 class="name"><%- name %></h4>\n' +
-            '        <h5><%- specialization %></h5>\n' +
-            '        <div class="teamMemberDetails" data-email="<%- email %>" data-phone="<%- phone %>" data-cell="<%- cell %>"></div></div>');
+        var templateRaw = $('#template').html();
+        var template = _.template(templateRaw);
 
         slickItem.className = 'slider-item';
         var destination = $('.slider-ajax');
-
-
 
         for (var i=0; i < teamArray.length; i++) {
             teamMemberInfo.src=teamArray[i].picture.large;
@@ -208,8 +180,5 @@ $(document).ready(function () {
             slickItem.innerHTML = template({'src': teamLocalImg[i], 'name': teamMemberInfo.name, 'specialization':teamSpecialization[i], 'email': teamMemberInfo.email, 'phone': teamMemberInfo.phone, 'cell': teamMemberInfo.cell});
             destination.append(slickItem.innerHTML);
         }
-        //slickItem.innerHTML = template({'src': teamMemberInfo.src, 'name': teamMemberInfo.name, 'email': teamMemberInfo.email, 'phone': teamMemberInfo.phone, 'cell': teamMemberInfo.cell});
-        //console.log(slickItem);
-        //$('#test').append(slickItem);
     }
 });
